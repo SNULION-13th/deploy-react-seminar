2fa4ec9bd80e5bf68dcbb1924c8dfa8b926c35ea
paul2126 <paul.mhryu@gmail.com>
2025-05-30 02:25:13 +0900

    week 10 hw

--- src/routes/SignUpPage.jsx
+++ src/routes/SignUpPage.jsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { useState } from "react";
+import { signUp } from "../apis/api";
 
 const SignUpPage = () => {
   const [signUpData, setSignUpData] = useState({
@@ -15,10 +17,26 @@ const SignUpPage = () => {
     setSignUpData({ ...signUpData, [id]: value });
   };
 
-  const handleSignUpSubmit = (e) => {
+  // const handleSignUpSubmit = async (e) => {
+  //   e.preventDefault(); // to prevent reloading the page
+  //   const response = await axios.post(
+  //     "http://localhost:8000/api/account/signup/",
+  //     signUpData,
+  //     {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       withCredentials: true,
+  //     }
+  //   );
+  //   console.log(response);
+
+  //   // console.log(signUpData);
+  //   // alert("회원가입 하기"); // TODO: add api call for sign up
+  // };
+  const handleSignUpSubmit = async (e) => {
     e.preventDefault(); // to prevent reloading the page
-    console.log(signUpData);
-    alert("회원가입 하기"); // TODO: add api call for sign up
+    signUp(signUpData);
   };
 
   return (
@@ -32,8 +50,8 @@ const SignUpPage = () => {
           required
           type="email"
           id="email"
-          className="input" 
-          value={signUpData.email} 
+          className="input"
+          value={signUpData.email}
           onChange={handleSignUpData}
         />
 
@@ -60,7 +78,6 @@ const SignUpPage = () => {
           value={signUpData.password}
           onChange={handleSignUpData}
         />
-
 
         <label htmlFor="confirm_password" className="label">
           *비밀번호 확인:
@@ -100,7 +117,11 @@ const SignUpPage = () => {
           <button type="reset" className="button mt-7">
             초기화
           </button>
-          <button type="submit" className="button mt-7">
+          <button
+            type="submit"
+            className="button mt-7"
+            onClick={handleSignUpSubmit}
+          >
             회원가입
           </button>
         </div>
